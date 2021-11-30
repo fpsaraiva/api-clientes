fc860078a9f4375ab58b83049e08ddeba049b86f
Fernando Saraiva <fpsaraiva@gmail.com>
2021-11-30 17:08:24 -0300

    refatoração: removidas classes do model para serem implementadas mais tarde

--- src/main/java/dev/fpsaraiva/apiclientes/model/repository/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.fpsaraiva.apiclientes.model.repository;
-
-import dev.fpsaraiva.apiclientes.model.entity.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
--- src/main/java/dev/fpsaraiva/apiclientes/model/repository/TelefoneRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.fpsaraiva.apiclientes.model.repository;
-
-import dev.fpsaraiva.apiclientes.model.entity.Telefone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
-}
